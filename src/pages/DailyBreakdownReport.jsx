5aff41ad1931e01dfd451af9a33cb41ccfe79bac
Rispan <rispanrobidigjaya0@gmail.com>
2025-12-09 17:13:02 +0700

    fix: bug

--- src/pages/DailyBreakdownReport.jsx
+++ src/pages/DailyBreakdownReport.jsx
@@ -81,7 +81,7 @@ const DailyBreakdownReport = () => {
 
   if (loading) {
     return (
-      <div className="flex-1 flex items-center justify-center">
+      <div className="flex-1 min-h-screen flex items-center justify-center">
         <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-primary-600"></div>
       </div>
     );
--- src/pages/admin/PartsMaster.jsx
+++ src/pages/admin/PartsMaster.jsx
@@ -120,7 +120,7 @@ const PartsMaster = () => {
 
   if (loading) {
     return (
-      <div className="flex-1 flex items-center justify-center">
+      <div className="flex-1 min-h-screen flex items-center justify-center">
         <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-primary-600"></div>
       </div>
     );
